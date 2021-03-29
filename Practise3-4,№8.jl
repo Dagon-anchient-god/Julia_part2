d977a96d6146af95edc960bbccde43be74dab679
Dagon-anchient-god <71090578+Dagon-anchient-god@users.noreply.github.com>
2021-03-29 20:29:50 +0300

    Add files via upload

---
+++ Practise3-4,№8.jl
@@ -0,0 +1,17 @@
+function myunique!(A)
+    sort!(A)
+end
+
+function myunique(A)
+end
+
+function allunique(A)
+    for i in 1:length(A)
+        for j in i+1:length(A)
+            if A[i]==A[j]
+                return false
+            end
+        end
+    end
+    return true
+end
